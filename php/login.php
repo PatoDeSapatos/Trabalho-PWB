d2bddf767b8383f140dee7c464aad8a4e7f2f19e
Pombo8 <117785799+Pombo8@users.noreply.github.com>
2023-04-12 18:39:09 -0300

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,5 +1,5 @@
 <?php
-    if(isset($_POST['user'])&& isset($_POST['pass'])){
+    if(isset($_POST['submitLogin'])){
         $user = $_POST['user'];
         $pass =$_POST['pass'];
         if($user=='teste' && $pass=='123456'){
