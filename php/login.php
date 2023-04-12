528bc992af2be90e6ec26cba12224d9ec831146e
Pato de Sapato <112494130+PatoDeSapatos@users.noreply.github.com>
2023-04-12 11:38:32 -0300

    css página de créditos

--- php/login.php
+++ php/login.php
@@ -2,7 +2,7 @@
     if(isset($_POST['user'])&& isset($_POST['pass'])){
         $user = $_POST['user'];
         $pass =$_POST['pass'];
-        if($user=='teste'&& $pass=='123456'){
+        if($user=='teste' && $pass=='123456'){
             header("location: postLogin.php");
         }
     }
