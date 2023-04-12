3e2600469f681761c92bd015762c9abf5ff54812
Pombo8 <117785799+Pombo8@users.noreply.github.com>
2023-04-12 18:29:20 -0300

    Update login.php

--- php/login.php
+++ php/login.php
@@ -3,7 +3,7 @@
         $user = $_POST['user'];
         $pass =$_POST['pass'];
         if($user=='teste' && $pass=='123456'){
-            header("location: postLogin.php");
+            header("location: ../html/postLogin.html");
         }
     }
 ?>
